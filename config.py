06dcce7d1a373bdbcc646e83351ff2b6eaf226a2
saibhossain <saibhossain5@gmail.com>
2026-06-18 14:24:08 +0600

    lreasing update

--- config.py
+++ config.py
@@ -3,18 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     
     # LangSmith Tracing Configuration
-    LANGCHAIN_TRACING_V2 = os.getenv("LANGSMITH_TRACING")
-    LANGCHAIN_ENDPOINT = os.getenv("LANGCHAIN_ENDPOINT")
-    LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
-    LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT")
+    LANGSMITH_TRACING = os.getenv("LANGSMITH_TRACING","true")
+    LANGCHAIN_ENDPOINT = os.getenv("LANGCHAIN_ENDPOINT", "https://api.smith.langchain.com")
+    LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY")
+    LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT","Email_assist")
 
     @classmethod
     def validate(cls):
         if not cls.OPENAI_API_KEY:
             raise ValueError("CRITICAL: OPENAI_API_KEY is missing from environment variables.")
-        if cls.LANGCHAIN_TRACING_V2 == "true" and not cls.LANGCHAIN_API_KEY:
+        if cls.LANGSMITH_TRACING == "true" and not cls.LANGSMITH_API_KEY:
             print("WARNING: LangSmith tracing is enabled but LANGCHAIN_API_KEY is missing.")
